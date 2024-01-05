49c1f8abc25f047bf97bf97698ce2d437980a0b8
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-05 02:25:37 +0100

    🎨 Task0 Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,16 +6,13 @@ def pascal_triangle(n):
     """Triangle"""
     if n <= 0:
         return []
-    pascal_triangle = [[1]]
+    pas = [[1]]
     for row_number in range(1, n):
         row = [1]
         for j in range(1, row_number):
-            element = (
-                pascal_triangle[row_number - 1][j - 1]
-                + pascal_triangle[row_number - 1][j]
-            )
+            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
             row.append(element)
         row.append(1)
-        pascal_triangle.append(row)
+        pas.append(row)
 
-    return pascal_triangle
+    return pas
