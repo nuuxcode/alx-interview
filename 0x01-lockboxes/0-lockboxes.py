dab3d2988baa9bebe2aed4f29c84cd5fd9309eb2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-05 22:56:17 +0100

    🎄 Task0 Lockboxes clean, with optimize idea in comment

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""BOXES BOXES"""
 
 
 def canUnlockAll(boxes):
@@ -12,35 +13,27 @@ def canUnlockAll(boxes):
                     and take the keys from it and add them to set of keys
                 keep loping through all setof keys
             ignore keys that dont have box
-            track opening of boxes by a counter, if at end it equal to lentgh of boxes it mean all boxes unlock
+            track opening of boxes by a counter, if at end it
+            equal to lentgh of boxes it mean all boxes unlock
+            OPTIMIZE IDEA :
+                if we add 0 to setofkeys at start, we dont need for in 23
     """
-    print("boxes:", boxes)
-    print("total boxes", len(boxes))
     setofkeys = []
     counter = 0
     total_boxes = len(boxes)
     for key in boxes[0]:
         if key < total_boxes and key not in setofkeys and key > 0:
             setofkeys.append(key)
-            counter +=1
+            counter += 1
     index = 0
     while index < len(setofkeys):
-        setkey = setofkeys[index]
-        #print("setofkeys", setofkeys)
-        #print("setofkeys length start:", len(setofkeys))
-        #print("key number:", setkey)
-        #print("opening box:", boxes[setkey])
         setkey = setofkeys[index]
         for key in boxes[setkey]:
             if key < total_boxes and key not in setofkeys and key > 0:
                 setofkeys.append(key)
-                counter +=1
+                counter += 1
         index += 1
-        #print("setofkeys", setofkeys)
-        #print("setofkeys length  end:", len(setofkeys))
-        #print("++++++")
-    print("total keys:", counter)
-    if (counter == total_boxes-1):
+    if counter == total_boxes - 1:
         return True
     else:
         return False
